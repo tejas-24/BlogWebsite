16919fbf7d6cc53d5fdf81052f31595d10e1cd7a
tejas-24 <70966635+tejas-24@users.noreply.github.com>
2022-07-10 23:33:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,65 +1,68 @@
-from flask import Flask, render_template, request, redirect
+
+from flask import Flask, render_template, request,session,redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-from flask_mail import Mail
+from datetime import  datetime
 import json
-from flask import session
+from flask_mail import Mail,Message
 import os
 from werkzeug.utils import secure_filename
 import math
 
-
-
-with open('config.json','r') as c:
-    params = json.load(c)["params"]
-
 local_server = True
 
+with open('config.json','r') as c :
+    params = json.load(c)["params"]
 
 app = Flask(__name__)
-
 app.secret_key = 'super-secret-key'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = params['gmail-user']
+app.config['MAIL_PASSWORD'] = params['gmail-password']
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USE_TLS'] = False
 
 app.config['UPLOAD_FOLDER'] = params['upload_location']
-# url for allow less secure apps
-#https://myaccount.google.com/lesssecureapps?pli=1&rapt=AEjHL4MR6jy4kOehbVFZ6eA1fJo9hQ3MXa385RAqWjPf4i_wcsSWycibjaQKollX3EBbrIaKPPwAYMFRryqFovZ0-N1WK3BFKQ
-app.config.update(
-    MAIL_SERVER = 'smtp.gmail.com',
-    MAIL_PORT = '465',
-    MAIL_USE_SSL = True,
-    MAIL_USERNAME = params['gmail-user'],
-    MAIL_PASSWORD=  params['gmail-password']
-)
+
 mail = Mail(app)
 
-if(local_server):
+
+if(local_server) :
     app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+'''
+this is beforer json file
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/instrument'
+'''
 
 db = SQLAlchemy(app)
 
-
 class Contacts(db.Model):
-    '''
-    sno, name phone_num, msg, date, email
-    '''
-    sno = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    phone_num = db.Column(db.String(12), nullable=False)
-    msg = db.Column(db.String(120), nullable=False)
-    date = db.Column(db.String(12), nullable=True)
-    email = db.Column(db.String(20), nullable=False)
+    '''srno, name, email, phone, message , date'''
+    srno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(20), unique=False, nullable=False)
+    phone = db.Column(db.String(12), unique=False, nullable=False)
+    message = db.Column(db.String(120), unique=False, nullable=False)
+    date = db.Column(db.String(12), unique=False,nullable=True)
 
 
 class Posts(db.Model):
-    sno = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    slug = db.Column(db.String(21), nullable=False)
-    content = db.Column(db.String(2000), nullable=False)
-    date = db.Column(db.String(12), nullable=True)
-    img_file = db.Column(db.String(12), nullable=True)
+    srno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=False, nullable=False)
+    slug = db.Column(db.String(25), unique=False, nullable=False)
+    content = db.Column(db.String(120), unique=False, nullable=False)
+    tagline = db.Column(db.String(120), unique=False, nullable=False)
+    date = db.Column(db.String(12), unique=False,nullable=True)
+    img = db.Column(db.String(25), unique=False, nullable=True)
 
+# this is before paginationa
+#
+# @app.route("/")
+# def home():
+#     posts = Posts.query.filter_by().all()[0:params['no_of_posts']]
+#     return render_template("index.html",params=params,posts=posts)
 
 @app.route("/")
 def home():
@@ -69,7 +72,8 @@ def home():
     if (not str(page).isnumeric()):
         page = 1
     page = int(page)
-    posts = posts[(page - 1) * int(params['no_of_posts']):(page - 1) * int(params['no_of_posts']) + int(params['no_of_posts'])]
+    posts = posts[(page - 1) * int(params['no_of_posts']):(page - 1) * int(params['no_of_posts']) + int(
+        params['no_of_posts'])]
     if page == 1:
         prev = "#"
         next = "/?page=" + str(page + 1)
@@ -82,137 +86,123 @@ def home():
 
     return render_template('index.html', params=params, posts=posts, prev=prev, next=next)
 
-@app.route("/allposts")
-def allposts():
-    posts = Posts.query.all()
-    return render_template('allposts.html', params=params, posts=posts)
+@app.route("/about")
+def about():
+    return render_template("about.html",params=params)
 
-@app.route("/dashboard" , methods=['GET', 'POST'])
+
+
+@app.route("/dashboard" ,methods=['GET','POST'])
 def dashboard():
-    if "user" in session and session['user']==params['admin_user']:
-        posts = Posts.query.all()
-        return render_template("dashboard.html", params=params, posts=posts)
 
-    if request.method=="POST":
+    if ('user' in session and session['user'] == params['admin_username']) :
+        posts = Posts.query.all()
+        return render_template('dashboard.html',params=params,posts=posts)
+
+    if request.method == "POST" :
         username = request.form.get("uname")
         userpass = request.form.get("pass")
-        if username==params['admin_user'] and userpass==params['admin_password']:
-            # set the session variable
-            session['user']=username
+        if(username == params['admin_username'] and userpass == params['admin_password']):
+            #set the session variable
+            session['user'] = username
             posts = Posts.query.all()
-            return render_template("dashboard.html", params=params, posts=posts)
-        else:
-            return render_template("login.html", params=params)
-    else:
-        return render_template("login.html", params=params)
+            return render_template('dashboard.html',params=params,posts = posts)
 
-# @app.route("/edit/<string:sno>", methods=['GET','POST'])
-# def edit(sno):
-#     if "user" in session and session['user'] == params['admin_user']:
-#         if request.method == 'POST':
-#             box_title = request.form.get('title')
-#             slug = request.form.get('slug')
-#             content = request.form.get('content')
-#             img_file = request.form.get('img_file')
-#             date = datetime.now()
-#
-#             if sno == '0':
-#                 post = Posts(title=box_title,slug=slug,content=content,img_file=img_file,date=date)
-#                 db.session.add(post)
-#                 db.session.commit()
-#             else:
-#                 post = Posts.query.filter_by(sno=sno).first()
-#                 post.box_title = box_title
-#                 post.slug = slug
-#                 post.content = content
-#                 post.img_file = img_file
-#                 post.date = date
-#                 db.session.commit()
-#                 return redirect('/edit/'+sno)
-#         post = Posts.query.filter_by(sno=sno).first()
-#         return render_template('edit.html', params=params, post=post)
 
-@app.route("/edit/<string:sno>", methods=['GET', 'POST'])
-def edit(sno):
-    if "user" in session and session['user'] == params['admin_user']:
-        if request.method == "POST":
+    return render_template("login.html", params=params)
+
+
+
+@app.route("/edit/<string:srno>" ,methods=['GET','POST'])
+def edit(srno):
+    if ('user' in session and session['user'] == params['admin_username']) :
+        if request.method == "POST" :
             box_title = request.form.get('title')
+            tagline = request.form.get('tagline')
             slug = request.form.get('slug')
             content = request.form.get('content')
-            img_file = request.form.get('img_file')
             date = datetime.now()
+            img = request.form.get('img')
 
-            if (sno == "0"):
-                post = Posts(title=box_title, slug=slug, content=content, img_file=img_file, date=date)
+            if srno == '0':
+                post = Posts(title=box_title,slug=slug,content=content,tagline=tagline,date=date, img=img)
+
                 db.session.add(post)
                 db.session.commit()
+
             else:
-                post = Posts.query.filter_by(sno=sno).first()
+                post = Posts.query.filter_by(srno = srno).first()
                 post.title = box_title
                 post.slug = slug
                 post.content = content
-                post.img_file = img_file
+                post.tagline = tagline
                 post.date = date
+                post.img = img;
                 db.session.commit()
+                return redirect('/edit/'+srno)
+        post = Posts.query.filter_by(srno=srno).first()
+        return render_template("edit.html",params=params,post=post,srno=srno)
 
-                return redirect("/edit/" + sno)
 
-        post = Posts.query.filter_by(sno=sno).first()
-        return render_template('edit.html', params=params, post=post)
-
-@app.route("/about")
-def about():
-    return render_template('about.html', params=params)
-
-@app.route("/post/<string:post_slug>", methods=['GET'])
-def post_route(post_slug):
-    post = Posts.query.filter_by(slug=post_slug).first()
-    return render_template('post.html', params=params, post=post)
-
-@app.route("/uploader", methods = ['GET', 'POST'])
+@app.route("/uploader",methods=['GET','POST'])
 def uploader():
-    if "user" in session and session['user']==params['admin_user']:
-        if request.method=='POST':
+    if ('user' in session and session['user'] == params['admin_username']):
+        if request.method == "POST" :
             f = request.files['file1']
-            f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
-            return "Uploaded successfully!"
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename) ))
+            return "Uploaded Successfull..."
+
+
+
 
 @app.route("/logout")
 def logout():
     session.pop('user')
     return redirect('/dashboard')
 
-@app.route("/delete/<string:sno>", methods=['GET','POST'])
-def delete(sno):
-    if "user" in session and session['user'] == params['admin_user']:
-        post = Posts.query.filter_by(sno=sno).first()
+
+@app.route("/delete/<string:srno>" ,methods=['GET','POST'])
+def Delete(srno) :
+    if ('user' in session and session['user'] == params['admin_username']):
+        post = Posts.query.filter_by(srno = srno).first()
         db.session.delete(post)
         db.session.commit()
-    return redirect("/dashboard")
+        return redirect("/dashboard")
 
 
-
-@app.route("/contact", methods = ['GET', 'POST'])
+@app.route("/contact",methods=['GET','POST'])
 def contact():
-    if(request.method == 'POST'):
-
-        '''Add entry to the database'''
+    if(request.method == 'POST') :
+        '''add entry to data base'''
         name = request.form.get('name')
         email = request.form.get('email')
-        phone = request.form.get('phone')
-        message = request.form.get('msg')
-        entry = Contacts(name=name, phone_num=phone, msg=message, date=datetime.now(), email=email)
-        # this is entry to database
+        phone_no = request.form.get('phone')
+        msg = request.form.get('message')
+
+        entry = Contacts(name=name,email=email,phone=phone_no,date=datetime.now(),message=msg)
         db.session.add(entry)
         db.session.commit()
-        # sending msg to email
-        mail.send_message('New message from ' + name,
-                          sender=email,
-                          recipients=[params['gmail-user']],
-                          body=message + "\n" + phone
-                          )
-
+        # e_msg = Message("new message from " + name,
+        #     sender = email,
+        #     recipients = [params['gmail-user']],
+        #     body = msg + "\n" + phone_no)
+        # mail.send(e_msg)
     return render_template('contact.html', params=params)
 
-app.run(debug=True)
 
+
+@app.route("/showpost")
+def showpost():
+    posts = Posts.query.filter_by().all()
+    return render_template("showpost.html",params=params, posts=posts)
+
+
+@app.route("/post/<string:post_slug>",methods = ['GET'])
+def post_route(post_slug):
+    post = Posts.query.filter_by(slug = post_slug).first()
+
+    return render_template("post.html",params=params,post=post)
+
+
+
+app.run(debug=True)
